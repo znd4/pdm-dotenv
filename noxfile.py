ad4982016ad7c8bab5ebb62da865019561703cb4
Zane Dufour <zane@znd4.me>
2023-04-15 13:21:56 -0400

    add code coverage step to ci + remove unsupported python versions

--- noxfile.py
+++ noxfile.py
@@ -4,8 +4,20 @@ import nox
 
 os.environ.update(PDM_IGNORE_SAVED_PYTHON="1", PDM_USE_VENV="1")
 
+PYTHON_VERSIONS = ("3.10", "3.11")
 
-@nox.session(python=("3.10", "3.11"))
+
+@nox.session(python=PYTHON_VERSIONS)
 def test(session):
+    _test(session, cov=False)
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def cov(session):
+    _test(session, cov=True)
+
+
+def _test(session: nox.Session, *, cov: bool) -> None:
     session.run("pdm", "install", "-Gtest", external=True)
-    session.run("pytest", "tests/")
+
+    session.run("pytest", *(["--cov=src"] if cov else []), "tests/")
